e78e982c5a1f65849890055ff3a67f8c0021ce89
Mila W. L. L <mila@lonsman.dk>
2023-02-07 19:55:32 +0100

    Commands are now saner

--- main.py
+++ main.py
@@ -78,87 +78,17 @@ async def set_prefix(ctx, alias):
         await ctx.send(f'Alias must be only one character!')
 
 
-@bot.command()
-async def point_giver(ctx, member: discord.Member):
-    if ctx.guild.owner_id == ctx.author.id:
-        session = base.Session()
-        user = session.query(PrivilegedUsers.user_id)\
-            .filter(PrivilegedUsers.server_id == ctx.guild.id)\
-            .filter(PrivilegedUsers.user_id == member.id)\
-            .one_or_none()
-        if user is None:
-            stmt = insert(PrivilegedUsers)\
-                .values(user_id=member.id, server_id=ctx.guild.id, allow_points=True)
-            session.execute(stmt)
-            session.commit()
-            await ctx.send(f'{member.name} provided with point privileges')
-        else:
-            await ctx.send(f'{member.name} already has point privileges')
-    else:
-        await ctx.send(f'Only the server owner is allowed to assign point giver role')
+@bot.group()
+async def points(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'You need to provide a subcommand')
 
 
-@bot.command()
-async def remove_point_giver(ctx, member: discord.Member):
-    if ctx.guild.owner_id == ctx.author.id:
-        session = base.Session()
-        stmt = update(PrivilegedUsers)\
-            .where(PrivilegedUsers.server_id == ctx.guild.id)\
-            .where(PrivilegedUsers.user_id == member.id)\
-            .values(allow_points=False)
-        session.execute(stmt)
-        session.commit()
-        await ctx.send(f'Successfully removed {member.name} as point giver')
-    else:
-        await ctx.send(f'Only the server owner can remove point givers')
-
-
-@bot.command()
-async def add_team(ctx, name, role: discord.Role):
-    if ctx.guild.owner_id == ctx.author.id:
-        session = base.Session()
-        team = session.query(Teams.team_id)\
-            .filter(Teams.team_id == role.id)\
-            .filter(Teams.server_id == ctx.guild.id)\
-            .one_or_none()
-        if team is None:
-            stmt = insert(Teams)\
-                .values(team_id=role.id, server_id=ctx.guild.id, name=name, point_total=0)
-            session.execute(stmt)
-            session.commit()
-            await ctx.send(f'Successfully created {name}')
-        else:
-            await ctx.send(f'A team using the role *{role.name}* already exists')
-    else:
-        await ctx.send(f'Only the server owner is allowed to create new teams')
-
-
-@bot.command()
-async def remove_team(ctx, role: discord.Role):
-    if ctx.guild.owner_id == ctx.author.id:
-        session = base.Session()
-        team = session.query(Teams.name)\
-            .filter(Teams.server_id == ctx.guild.id)\
-            .filter(Teams.team_id == role.id)\
-            .one_or_none()
-        if team is None:
-            await ctx.send(f'Team using role {role.name} does not exist')
-        else:
-            stmt = delete(Teams)\
-                .filter(Teams.server_id == ctx.guild.id)\
-                .filter(Teams.team_id == role.id)
-            session.execute(stmt)
-            session.commit()
-            await ctx.send(f'Successfully removed {team[0]}')
-    else:
-        await ctx.send(f'Only the server owner is allowed to remove teams')
-
-
-@bot.command()
+@points.command()
 async def leaderboard(ctx, argument='team'):
     session = base.Session()
     if argument.lower() not in ('team', 'user'):
-        await ctx.send(f'Please pick a valid choice')
+        await ctx.send(f'Please pick a valid choice - either team or user')
     else:
         if argument.lower() == 'team':
             board = session.query(Teams.name, Teams.point_total)\
@@ -176,8 +106,114 @@ async def leaderboard(ctx, argument='team'):
             await ctx.send(await util.pretty_print_leaderboard(board, ctx))
 
 
-@bot.command()
-async def individual_points(ctx, member: discord.Member, points):
+@points.group()
+async def mod(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'You need to provide a subcommand add or remove')
+
+
+@mod.command()
+async def add(ctx, member: discord.Member = None):
+    if member is None:
+        await ctx.send(f'You need to provide a server member')
+    else:
+        if ctx.guild.owner_id == ctx.author.id:
+            session = base.Session()
+            user = session.query(PrivilegedUsers.user_id)\
+                .filter(PrivilegedUsers.server_id == ctx.guild.id)\
+                .filter(PrivilegedUsers.user_id == member.id)\
+                .one_or_none()
+            if user is None:
+                stmt = insert(PrivilegedUsers)\
+                    .values(user_id=member.id, server_id=ctx.guild.id, allow_points=True)
+                session.execute(stmt)
+                session.commit()
+                await ctx.send(f'{member.name} provided with point privileges')
+            else:
+                await ctx.send(f'{member.name} already has point privileges')
+        else:
+            await ctx.send(f'Only the server owner is allowed to assign point giver role')
+
+
+@mod.command()
+async def remove(ctx, member: discord.Member = None):
+    if member is None:
+        await ctx.send(f'You need to provide a server member')
+    else:
+        if ctx.guild.owner_id == ctx.author.id:
+            session = base.Session()
+            stmt = update(PrivilegedUsers)\
+                .where(PrivilegedUsers.server_id == ctx.guild.id)\
+                .where(PrivilegedUsers.user_id == member.id)\
+                .values(allow_points=False)
+            session.execute(stmt)
+            session.commit()
+            await ctx.send(f'Successfully removed {member.name} as point giver')
+        else:
+            await ctx.send(f'Only the server owner can remove point givers')
+
+
+@points.group()
+async def team(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'You need to provide a subcommand - add - remove - award')
+
+
+@team.command()
+async def add(ctx, name, role: discord.Role):
+    if ctx.guild.owner_id == ctx.author.id:
+        session = base.Session()
+        team_obj = session.query(Teams.team_id)\
+            .filter(Teams.team_id == role.id)\
+            .filter(Teams.server_id == ctx.guild.id)\
+            .one_or_none()
+        if team_obj is None:
+            stmt = insert(Teams)\
+                .values(team_id=role.id, server_id=ctx.guild.id, name=name, point_total=0)
+            session.execute(stmt)
+            session.commit()
+            await ctx.send(f'Successfully created {name}')
+        else:
+            await ctx.send(f'A team using the role *{role.name}* already exists')
+    else:
+        await ctx.send(f'Only the server owner is allowed to create new teams')
+
+
+@team.command()
+async def remove(ctx, role: discord.Role):
+    if ctx.guild.owner_id == ctx.author.id:
+        session = base.Session()
+        team_obj = session.query(Teams.name)\
+            .filter(Teams.server_id == ctx.guild.id)\
+            .filter(Teams.team_id == role.id)\
+            .one_or_none()
+        if team_obj is None:
+            await ctx.send(f'Team using role {role.name} does not exist')
+        else:
+            stmt = delete(Teams)\
+                .filter(Teams.server_id == ctx.guild.id)\
+                .filter(Teams.team_id == role.id)
+            session.execute(stmt)
+            session.commit()
+            await ctx.send(f'Successfully removed {team_obj[0]}')
+    else:
+        await ctx.send(f'Only the server owner is allowed to remove teams')
+
+
+@team.command()
+async def award(ctx, role: discord.Role, points_amount):
+    session = base.Session()
+    privileged = util.check_privilege(session=session, ctx=ctx, points=True, teams=False)
+    if privileged is False and ctx.guild.owner_id != ctx.author.id:
+        await ctx.send(f'You are not allowed to update points')
+    else:
+        message_content = util.update_team_total(session=session, ctx=ctx, points=points_amount, team_name=role.name,
+                                                 team_id=role.id)
+        await ctx.send(message_content)
+
+
+@points.command()
+async def award(ctx, member: discord.Member, points_amount):
     session = base.Session()
     privileged = util.check_privilege(session=session, ctx=ctx, points=True, teams=False)
     if privileged is False and ctx.guild.owner_id != ctx.author.id:
@@ -190,49 +226,38 @@ async def individual_points(ctx, member: discord.Member, points):
         team_id_and_name = util.check_team(session=session, ctx=ctx, member=member)
         stmt = insert(UserPoints)\
             .values(user_id=member.id, team_id=team_id_and_name[0],
-                    point_total=points, server_id=ctx.guild.id)
+                    point_total=points_amount, server_id=ctx.guild.id)
         session.execute(stmt)
         session.commit()
-        message_content = f'Added {points} to {member.display_name} which is their new total'
+        message_content = f'Added {points_amount} to {member.display_name} which is their new total'
         if team_id_and_name is not None:
             session = base.Session()
-            message_content = util.update_team_total(session=session, ctx=ctx, points=points,
+            message_content = util.update_team_total(session=session, ctx=ctx, points=points_amount,
                                                      team_name=team_id_and_name[0][1], team_id=team_id_and_name[0][0],
                                                      message_content=message_content)
         await ctx.send(message_content)
     else:
-        total = member_points_call[0] + int(points)
+        total = member_points_call[0] + int(points_amount)
         stmt = update(UserPoints)\
             .where(UserPoints.server_id == ctx.guild.id)\
             .where(UserPoints.user_id == member.id).values(point_total=total)
         session.execute(stmt)
         session.commit()
-        message_content = f'Added {points} to {member.display_name} their new total is {total}'
+        message_content = f'Added {points_amount} to {member.display_name} their new total is {total}'
 
         if member_points_call[1] is not None:
             session = base.Session()
             team_id_and_name = util.check_team(session=session, ctx=ctx, member=member)
             message_content = util.update_team_total(session=session, ctx=ctx, team_id=team_id_and_name[0][0],
-                                                     team_name=team_id_and_name[0][1], points=points,
+                                                     team_name=team_id_and_name[0][1], points=points_amount,
                                                      message_content=message_content)
-        await ctx.send(message_content)
-
-
-@bot.command()
-async def update_points(ctx, role: discord.Role, points):
-    session = base.Session()
-    privileged = util.check_privilege(session=session, ctx=ctx, points=True, teams=False)
-    if privileged is False and ctx.guild.owner_id != ctx.author.id:
-        await ctx.send(f'You are not allowed to update points')
-    else:
-        message_content = util.update_team_total(session=session, ctx=ctx, points=points, team_name=role.name, team_id=role.id)
         await ctx.send(message_content)
 
 
 @bot.group()
 async def quote(ctx):
     if ctx.invoked_subcommand is None:
-        await ctx.send(f'You need to provide a subcommand')
+        await ctx.send(f'You need to provide a subcommand - add - remove - random - show')
 
 
 @quote.command()
